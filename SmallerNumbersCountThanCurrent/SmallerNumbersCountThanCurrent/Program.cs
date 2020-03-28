7e7b031ecf32687512be50b01cb0ec88d66392d8
Ali UÇAR <ali.ucar@avl.com>
2020-03-29 01:40:16 +0300

    Smaller Numbers Than Current

---
+++ SmallerNumbersCountThanCurrent/SmallerNumbersCountThanCurrent/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallerNumbersCountThanCurrent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArray = new int[] { 8, 1, 2, 3, 3, 5 };
+            int[] resultArray = Solution.SmallerNumbersThanCurrent(testArray);
+
+            for (int i = 0; i < testArray.Length; i++)
+            {
+                Console.WriteLine($"{testArray[i]} , {resultArray[i]}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SmallerNumbersCountThanCurrent/SmallerNumbersCountThanCurrent/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallerNumbersCountThanCurrent
+{
+    class Solution
+    {
+        public static int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result[i] = 0;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] != nums[j])
+                    {
+                        if (nums[i] > nums[j])
+                        {
+                            result[i] = result[i]++;
+                        }
+                        else
+                        {
+                            result[j]++;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
